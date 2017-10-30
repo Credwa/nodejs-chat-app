29f3e1feb62cbc5fd8d7f72ff6ffad6088f557d8
Craig Edwards <craigroe7@gmail.com>
2017-10-30 17:30:03 -0400

    fix backend url

--- public/src/main.js
+++ public/src/main.js
@@ -4,7 +4,8 @@ import VueSocketIO from 'vue-socket.io';
 import VueMaterial from 'vue-material'
 
 Vue.use(VueMaterial)
-Vue.use(VueSocketIO, 'http://localhost:3000');
+// Vue.use(VueSocketIO, 'http://localhost:3000');
+Vue.use(VueSocketIO, 'https://shielded-wave-68392.herokuapp.com/');
 
 Vue.material.registerTheme('default', {
   primary: {
